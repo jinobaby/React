3f9cfe6115c4064b74145abdab75108939f692da
Jino Baby <jinobaby.conestoga@gmail.com>
2025-05-07 02:54:51 -0400

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <section className='green-main'></section>
+
+    </>
+  )
+}
+
+export default App
